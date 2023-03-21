4446fe61e8182f4a30904be81d136fa0059aa8d7
Agustin Bonilla <agustin.bonilla@horus.com.uy>
2023-03-21 01:08:39 -0300

    Update sample pages

--- SkeletonSample/Pages/NewPage1.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SkeletonSample.Pages;
-
-public partial class NewPage1 : ContentPage
-{
-	public NewPage1()
-	{
-		InitializeComponent();
-	}
-}
--- SkeletonSample/Pages/VerticalShake.xaml.cs
+++ SkeletonSample/Pages/VerticalShake.xaml.cs
@@ -16,11 +16,6 @@ namespace SkeletonSample.Pages
 
             if (BindingContext is BaseViewModel viewModel)
                 viewModel.LoadCommand.Execute(null);
-
-            //if (Microsoft.Maui.Devices.DeviceInfo.Platform.Equals(Microsoft.Maui.Devices.DevicePlatform.iOS))
-            //    mainGrid.Margin = new Thickness(30, On<Xamarin.Forms.PlatformConfiguration.iOS>().SafeAreaInsets().Top + 30, 30, 30);
-            //else
-            //    mainGrid.Margin = new Thickness(30, 50, 30, 30);
         }
     }
 }
