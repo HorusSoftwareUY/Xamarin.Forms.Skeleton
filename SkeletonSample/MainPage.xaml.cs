fe5e164b6ae2100696408a311a4df0139368341f
Agustin Bonilla <agustin.bonilla@horus.com.uy>
2023-03-21 11:08:43 -0300

    Change app icon and splash screen

--- SkeletonSample/MainPage.xaml.cs
+++ SkeletonSample/MainPage.xaml.cs
@@ -15,25 +15,3 @@ public partial class MainPage : TabbedPage
         this.Children.Add(new CustomAnimation());
     }
 }
-
-//public partial class MainPage : ContentPage
-//{
-//	int count = 0;
-
-//	public MainPage()
-//	{
-//		InitializeComponent();
-//	}
-
-//	private void OnCounterClicked(object sender, EventArgs e)
-//	{
-//		count++;
-
-//		if (count == 1)
-//			CounterBtn.Text = $"Clicked {count} time";
-//		else
-//			CounterBtn.Text = $"Clicked {count} times";
-
-//		SemanticScreenReader.Announce(CounterBtn.Text);
-//	}
-//}
